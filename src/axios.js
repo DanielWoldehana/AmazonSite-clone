49234a12a2b6e0dea210cba47d0d18989100a6f8
Daniel Woldehana <danielwoldehana@MacBook-Air.local>
2021-04-07 17:59:51 -0400

    checkout and subtotal responsive

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:5001/fir-clone-f64a5/us-central1/api"
-  baseURL: "https://us-central1-fir-clone-f64a5.cloudfunctions.net/api", //The API (cloud funtion) URL
+  baseURL: "http://localhost:5001/fir-clone-f64a5/us-central1/api",
+  // baseURL: "https://us-central1-fir-clone-f64a5.cloudfunctions.net/api", //The API (cloud funtion) URL
 });
+
+//to run locally run command firebase emulators:start
 
 export default instance;
