22c3e203da87d60eb6037722bfb3ca899accd574
Daniel Woldehana <danielwoldehana@MacBook-Air.local>
2021-04-08 02:24:52 -0400

    responsive css edit completed

--- src/Components/Orders/Order.js
+++ src/Components/Orders/Order.js
@@ -7,11 +7,14 @@ import "./Order.css";
 const Order = ({ order }) => {
   return (
     <div className="order">
-      <h2>Order</h2>
-      <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
-      <p className="order__id">
-        <small>{order.id}</small>
-      </p>
+      <div className="order__title">
+        <h2>Order</h2>
+        <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
+        <p className="order__id">
+          <small>{order.id}</small>
+        </p>
+      </div>
+
       {order.data.basket?.map((item) => (
         <CheckoutProduct
           id={item.id}
